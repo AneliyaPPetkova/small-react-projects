9c7d81e316471450434827d1251579b93ce42739
AneliyaPPetkova <AneliyaP.Petkova@gmail.com>
2023-02-10 20:38:53 +0200

    02.Tours task - Add show more/show less functionality

--- src/components/Tours/TourCard.tsx
+++ src/components/Tours/TourCard.tsx
@@ -1,19 +1,24 @@
-import { Tour } from "./Types"
+import { useState } from "react";
+import { Tour } from "./Types";
 
-const TourCard = ({id, name, image, info, price}: Tour) => {
-    return (
-        <article className="single-tour">
-            <img src={image} alt={name} />
-            <footer>
-                <div className="tour-info">
-                    <h4>{name}</h4>
-                    <h4 className="tour-price">${price}</h4>
-                </div>
-                <p>{info}</p>
-                <button className="delete-btn">Not interested</button>
-            </footer>
-        </article>
-    )
-}
+const TourCard = ({ id, name, image, info, price }: Tour) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>{readMore ? 'Show Less' : 'Show More'}</button>
+        </p>
+        <button className="delete-btn">Not interested</button>
+      </footer>
+    </article>
+  );
+};
 
 export default TourCard;
